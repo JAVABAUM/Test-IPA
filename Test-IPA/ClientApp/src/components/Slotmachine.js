ebf2e9ec5524b693a06fca0c0888ab686863446f
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2022-01-20 08:28:52 +0100

    slot-machine

---
+++ Test-IPA/ClientApp/src/components/Slotmachine.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Slotmachine extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the Javabaum Casino!</h1>
+                <br></br>
+                <h4>Slot-machine!!</h4>
+            </div>
+        );
+    }
+}
