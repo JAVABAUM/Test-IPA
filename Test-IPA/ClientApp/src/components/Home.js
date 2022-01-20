1cbf8e9956dd5d6b4506e458a607909c0b2a2e3b
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2022-01-20 08:11:17 +0100

    delete unused files

--- Test-IPA/ClientApp/src/components/Home.js
+++ Test-IPA/ClientApp/src/components/Home.js
@@ -6,7 +6,7 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hallo mini Javabaum Kraecks!</h1>
+        <h1>Welcome to the Javabaum Casino!</h1>
       </div>
     );
   }
